0369330a018c0356a2bd6dbe35e26b0c12271f36
isupden <isupden@yandex.ru>
2022-09-13 05:07:38 -0300

    fix

--- js/validate.js
+++ js/validate.js
@@ -1,15 +1,17 @@
 function updateY() {
-    let separator = '.';
+    let separator1 = '.';
+    let separator2 = ',';
     let maxValue = 5;
     let minValue = -5;
     let valid = false;
     let value = NaN;
-
-    if (this.value[0] === separator) {
+    if (this.value[0] === separator1 || this.value[0] === separator2) {
         this.value = '0' + this.value;
     }
-    if (isFinite(this.value)) {
-        value = parseFloat(this.value);
+    let y = this.value.replace(',', '.')
+
+    if (isFinite(y)) {
+        value = parseFloat(y);
     }
     if (!isNaN(value)) {
         if (value > minValue && value < maxValue) {
@@ -17,6 +19,12 @@ function updateY() {
             moveY(value);
         }
     }
+    if (valid || this.value === '') {
+        document.getElementById('inputY').style.border = "";
+    } else {
+        document.getElementById('inputY').style.border = "3px solid #ff1867";
+    }
+
     $('#submit-button').attr('disabled', !valid);
 }
 
